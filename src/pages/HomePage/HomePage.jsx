b15343edbd9684a6ea4f9be071725e41618563d9
AfanasevKirillGitHub <106386403+AfanasevKirillGitHub@users.noreply.github.com>
2023-01-25 17:20:46 +0200

    add responsive carousel

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -8,7 +8,17 @@ import {
   MovieList,
   MovieTitle,
   Title,
+  TrendWrapper
 } from './HomePage.styled';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/navigation";
+
+import "../../index.css";
+
+
 
 export const HomePage = () => {
   const [trendingMovies, setTrendingMovies] = useState([]);
@@ -28,23 +38,53 @@ export const HomePage = () => {
   }, []);
 
   return (
-    <Box maxWidth={1400} py={140} mx="auto">
-      <Title>Trending movies today</Title>
-      <MovieList>
-        {trendingMovies.map(({ id, title, poster_path }) => {
-          return (
-            <MovieItem key={id}>
-              <MovieLink to={`movies/${id}`}>
-                <MovieImage
-                  src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
-                  alt={title}
-                />
-                <MovieTitle>{title}</MovieTitle>
-              </MovieLink>
-            </MovieItem>
-          );
-        })}
-      </MovieList>
+
+    <Box>
+      <TrendWrapper>
+        <Title>Trending movies today</Title>
+        <MovieList>
+          <Swiper
+            loop={true}
+            loopFillGroupWithBlank={true}
+            breakpoints={{
+              320: {
+                slidesPerView: 3,
+                spaceBetween: 10,
+                slidesPerGroup: 3,
+              },
+              768: {
+                slidesPerView: 5,
+                spaceBetween: 10,
+                slidesPerGroup: 5,
+              },
+              1280: {
+                slidesPerView: 7,
+                spaceBetween: 25,
+                slidesPerGroup: 7,
+              },
+            }}
+            navigation={true}
+            modules={[Navigation]}
+          >
+            {trendingMovies.map(({ id, title, poster_path }) => {
+              return (
+                <SwiperSlide key={id}>
+                  <MovieItem>
+                    <MovieLink to={`movies/${id}`}>
+                      <MovieImage
+                        src={`https://image.tmdb.org/t/p/w300/${poster_path}`}
+                        alt={title}
+                      />
+                      {/* <MovieTitle>{title}</MovieTitle> */}
+                    </MovieLink>
+                  </MovieItem>
+                </SwiperSlide>
+              );
+            })}
+          </Swiper>
+        </MovieList>
+      </TrendWrapper>
     </Box>
+
   );
 };
--- src/pages/HomePage/HomePage.styled.jsx
+++ src/pages/HomePage/HomePage.styled.jsx
@@ -2,58 +2,73 @@ import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 export const Title = styled.h2`
-  text-align: center;
-  margin: 0 0 20px 0;
+  margin: 0;
+  padding-left: 10px;
   color: #fff;
-
   font-family: 'Ubuntu';
   font-style: normal;
   font-weight: 400;
-  font-size: 32px;
+  font-size: 24px;
   line-height: 37px;
 `;
 
 export const MovieList = styled.ul`
-  padding: 0;
-  margin: 0;
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 25px;
-  list-style: none;
-  text-align: center;
+    padding: 0;
+    margin: 0;
+    list-style: none;
 `;
 
 export const MovieItem = styled.li`
-  background-color: #ffffff30;
-  width: 231px;
-  border-radius: 4px;
   transition: all 0.25s ease-out;
+
   :hover {
     transform: scale(1.02);
     transition: all 0.25s ease-out;
-    box-shadow: 0 14px 28px rgba(255, 60, 82, 0.25),
-      0 10px 10px rgba(255, 60, 82, 0.25);
+    box-shadow: 0 5px 20px rgb(255 60 82 / 25%), 0 10px 20px rgb(255 60 82 / 25%)
   }
 `;
 
 export const MovieLink = styled(Link)`
-  display: block;
   text-decoration: none;
-  color: #fff;
-  transition: all 0.25s ease-out;
-  :hover {
-    transition: all 0.25s ease-out;
-  }
 `;
 
 export const MovieImage = styled.img`
+  border-radius: 4px;
   display: block;
-  width: 231px;
-  border-radius: 4px 4px 0 0;
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
 `;
 
-export const MovieTitle = styled.p`
-  margin: 0;
-  padding: 10px 0 10px 0;
-`;
+// export const MovieTitle = styled.p`
+//   margin: 0;
+//   padding: 4px 0 4px 0;
+//   font-family: 'Ubuntu';
+//   font-style: normal;
+//   font-weight: 400;
+//   font-size: 9px;
+
+//   @media (min-width: 768px) {
+//      padding: 10px 0 10px 0;
+//   }
+//    @media (min-width: 1024px) {
+//   font-size: 14px;
+//   }
+//   `
+
+export const TrendWrapper = styled.div`
+padding-left: 12px;
+padding-right: 12px;
+padding-top: 140px;
+padding-bottom: 40px;
+margin: 0 auto;
+
+@media (min-width: 768px) {
+  padding-left: 20px;
+  padding-right: 20px;
+}
+  @media (min-width: 1024px) {
+  padding-left: 50px;
+  padding-right: 50px;
+}
+`
